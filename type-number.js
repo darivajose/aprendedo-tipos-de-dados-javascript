874b3a864092e430bd80b07670fd413301f4f3c9
Jose Dariva <josedariva@hotmail.com>
2026-02-26 15:45:50 -0300

    Aprendendo type-number e type-string javascript

---
+++ type-number.js
@@ -0,0 +1,35 @@
+// tipo Number 
+// ponto flutuante
+// NaN -> Not a Number (não é um número)
+//string -> texto
+
+//const meuNumero = 3;
+
+//const primeiroNumero = Number(prompt("Digite o primeiro número:"));
+//const segundoNumero = Number(prompt("Digite o segundo número:"));
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoAdicao = primeiroNumero + segundoNumero;
+const operacaoDivisao = primeiroNumero / segundoNumero;
+const operacaoMultiplicacao = primeiroNumero * segundoNumero;
+const operacaoSubtracao = primeiroNumero - segundoNumero;
+
+console.log("O resultado da adição é: " + operacaoAdicao);
+console.log("O resultado da divisão é: " + operacaoDivisao);
+console.log("O resultado da multiplicação é: " + operacaoMultiplicacao);
+console.log("O resultado da subtração é: " + operacaoSubtracao);
+
+// ponto flutuante
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log("O resultado é:" + novaOperacao);
+
+ // NaN
+const nome = "José";
+console.log(nome * primeiroNumero);  
+
---
+++ type-string.js
@@ -0,0 +1,14 @@
+// string -> texto
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senha1234";
+
+console.log(texto1);
+console.log(texto2);
+console.log(senha);
+
+//concatenacao -> juntar textos
+const alura = "Alura";
+console.log(alura, "é a numero: " + primeiroNumero);
+
